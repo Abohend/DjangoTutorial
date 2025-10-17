b7bd66f4a729120a1f03dc706d2c070502e7d791
Mohamed Abohend <mhmdabohend@gmail.com>
2025-10-17 15:25:07 +0300

    project setup and created db along with simples views

---
+++ polls/views.py
@@ -0,0 +1,22 @@
+from django.http import Http404, HttpResponse
+from django.shortcuts import render
+from .models import *
+
+def index(request):
+    latest_questions_list = Question.objects.order_by("-pub_date")[0:5]
+    context = {"latest_questions_list": latest_questions_list}
+    return render(request, "polls/index.html", context)
+
+def details(request, question_id):
+    try:
+        question = Question.objects.get(pk = question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    
+    return render(request, "polls/details.html", {"question": question})
+
+def results(request, question_id):
+    return HttpResponse("You're looking at the results of question %s" % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s" % question_id)
